0f753a6b3bf2c731fd6be0cff87cd3f86f981fda
C. Corey Capel <corey@capelio.com>
2014-08-19 01:20:38 -0400

    Rename fixtures and move into individual tests for consistency

--- test/index.js
+++ test/index.js
@@ -2,32 +2,41 @@ var test = require('tape');
 var _ = require('lodash');
 _.mixin(require('../lib'));
 
-var fixture = {
-  str: 'bar',
-  num: 10,
-  deep: {
-    object: {
-      str: 'bar',
-      num: 10
-    }
-  }
-};
-
 test('hasPath', function (t) {
-  t.true(_.hasPath(fixture, 'str'), 'Finds shallow property');
-  t.false(_.hasPath(fixture, 'foo'), 'Cannot find non-existant shallow property');
-  t.true(_.hasPath(fixture, 'deep.object.str'), 'Finds deep property');
-  t.false(_.hasPath(fixture, 'deep.object.foo'), 'Cannot find non-existant deep property');
+  var object = {
+    str: 'bar',
+    deep: {
+      object: {
+        str: 'bar'
+      }
+    }
+  };
+
+  t.true(_.hasPath(object, 'str'), 'Finds shallow property');
+  t.false(_.hasPath(object, 'foo'), 'Cannot find non-existant shallow property');
+  t.true(_.hasPath(object, 'deep.object.str'), 'Finds deep property');
+  t.false(_.hasPath(object, 'deep.object.foo'), 'Cannot find non-existant deep property');
   t.end();
 });
 
 test('hasNumber', function (t) {
-  t.true(_.hasNumber(fixture, 'num'), 'Finds shallow number');
-  t.false(_.hasNumber(fixture, 'str'), 'Finds and returns false for shallow string');
-  t.false(_.hasNumber(fixture, 'foo'), 'Cannot find non-existant shallow number');
-  t.true(_.hasNumber(fixture, 'deep.object.num'), 'Finds deep number');
-  t.false(_.hasNumber(fixture, 'deep.object.str'), 'Finds and returns false for deep string');
-  t.false(_.hasNumber(fixture, 'deep.object.foo'), 'Cannot find non-existant deep number');
+  var object = {
+    str: 'foo',
+    num: 10,
+    deep: {
+      object: {
+        str: 'foo',
+        num: 10
+      }
+    }
+  };
+
+  t.true(_.hasNumber(object, 'num'), 'Finds shallow number');
+  t.false(_.hasNumber(object, 'str'), 'Finds and returns false for shallow string');
+  t.false(_.hasNumber(object, 'foo'), 'Cannot find non-existant shallow number');
+  t.true(_.hasNumber(object, 'deep.object.num'), 'Finds deep number');
+  t.false(_.hasNumber(object, 'deep.object.str'), 'Finds and returns false for deep string');
+  t.false(_.hasNumber(object, 'deep.object.foo'), 'Cannot find non-existant deep number');
   t.end();
 });
 
